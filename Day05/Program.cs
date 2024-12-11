685d3534d53e9be4edf411c82532a5386c93aac7
Daniel <d.o.rasmusson@gmail.com>
2024-12-11 09:25:14 +0100

    Day five, part one

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -5,9 +5,36 @@ var pagesList = GetPagesList(input);
 
 var partOne = PartOne(orderingRules, pagesList);
 Console.WriteLine($"Part one: {partOne}");
-int PartOne(List<(int, int)> valueTuples, List<List<int>> list)
+int PartOne(List<(int, int)> orderingRules, List<List<int>> pagesList)
 {
-    return 0;
+    var result = 0;
+    foreach (var pages in pagesList)
+    {
+        var matchingOrderingRules = GetMatchingOrderingRules(orderingRules, pages);
+        if (PagesAreInOrder(pages, matchingOrderingRules)) result += pages[pages.Count / 2];
+    }
+    return result;
+}
+bool PagesAreInOrder(List<int> pages, List<(int, int)> orderingRules)
+{
+    foreach (var orderingRule in orderingRules)
+    {
+        if (!(pages.IndexOf(orderingRule.Item1) < pages.IndexOf(orderingRule.Item2))) return false;
+    }
+
+    return true;
+}
+
+List<(int, int)> GetMatchingOrderingRules(List<(int, int)> orderingRules, List<int> pages)
+{
+    var matchingOrderingRules = new List<(int, int)>();
+
+    foreach (var orderingRule in orderingRules)
+    {
+        if (pages.Contains(orderingRule.Item1) && pages.Contains(orderingRule.Item2))
+            matchingOrderingRules.Add(orderingRule);
+    }
+    return matchingOrderingRules;
 }
 
 List<List<int>> GetPagesList(string[] strings)

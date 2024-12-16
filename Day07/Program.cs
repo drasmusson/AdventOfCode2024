a0da86cef73db2a76930fbb965846181e056e016
Daniel <d.o.rasmusson@gmail.com>
2024-12-16 11:30:28 +0100

    day seven done

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,29 +1,64 @@
-﻿var input  = System.IO.File.ReadAllLines("Input.txt");
+﻿using System.Diagnostics;
 
+var input  = System.IO.File.ReadAllLines("Input.txt");
+
+var sw = new Stopwatch();
+sw.Start();
 var partOne = PartOne(input);
+sw.Stop();
 Console.WriteLine($"Part One: {partOne}");
-
+Console.WriteLine($"Time taken: {sw.ElapsedMilliseconds} ms");
+sw.Restart();
+var partTwo = PartTwo(input);
+sw.Stop();
+Console.WriteLine($"Part Two: {partTwo}");
+Console.WriteLine($"Time taken: {sw.ElapsedMilliseconds} ms");
 long PartOne(string[] strings)
 {
     var equations = ParseEquations(strings);
     var result = 0L;
     foreach (var equation in equations)
     {
-        var masks = GetMasks(equation.Value.Count - 1);
+        var masks = GetMasks(equation.Item2.Count - 1);
         var match = false;
         var i = 0;
         while (!match && i <= masks.Count - 1)
         {
             var mask = masks[i];
-            if (equation.Key == ApplyMask(equation.Value, mask))
+            if (equation.Item1 == ApplyMask(equation.Item2, mask))
             {
-                result += equation.Key;
+                result += equation.Item1;
                 match = true;
             }
             
             i++;
         }
     }
+    return result;
+}
+
+long PartTwo(string[] strings)
+{
+    var equations = ParseEquations(strings);
+    var result = 0L;
+    foreach (var equation in equations)
+    {
+        var masks = GetMasks2(equation.Item2.Count - 1);
+        var match = false;
+        var i = 0;
+        while (!match && i <= masks.Count - 1)
+        {
+            var mask = masks[i];
+            if (equation.Item1 == ApplyMask(equation.Item2, mask))
+            {
+                result += equation.Item1;
+                match = true;
+            }
+            
+            i++;
+        }
+    }
+
     return result;
 }
 
@@ -34,26 +69,34 @@ long ApplyMask(List<int> equationValue, int[] ints)
     {
         var num = ints[i];
 
-        if (num == 0)
-            total += equationValue[i + 1];
-
-        if (num == 1)
-            total *= equationValue[i + 1];
-
+        switch (num)
+        {
+            case 0:
+                total += equationValue[i + 1];
+                break;
+            case 1:
+                total *= equationValue[i + 1];
+                break;
+            case 2:
+                total = Concat(total, equationValue[i + 1]);
+                break;
+        }
     }
     return total;
 }
 
-Dictionary<long, List<int>> ParseEquations(string[] strings)
+long Concat(long a, int b) => long.Parse($"{a}{b}");
+
+List<(long, List<int>)> ParseEquations(string[] strings)
 {
-    var equations = new Dictionary<long, List<int>>();
+    var equations = new List<(long, List<int>)>();
 
     foreach (var s in strings)
     {
         var split = s.Split(": ");
         var testValue = long.Parse(split[0]);
         var operators = split[1].Split(" ").Select(int.Parse).ToList();
-        equations.Add(testValue, operators);
+        equations.Add((testValue, operators));
     }
     return equations;
 }
@@ -73,5 +116,29 @@ List<int[]> GetMasks(int length)
         result.Add(combination);
     }
 
+    return result;
+}
+
+List<int[]> GetMasks2(int length)
+{
+    var result = new List<int[]>();
+    var current = new int[length];
+        
+    void Generate(int position)
+    {
+        if (position == length)
+        {
+            result.Add((int[])current.Clone());
+            return;
+        }
+            
+        for (int num = 0; num <= 2; num++)
+        {
+            current[position] = num;
+            Generate(position + 1);
+        }
+    }
+        
+    Generate(0);
     return result;
 }

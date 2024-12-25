c348a363040cf5bbf90e2f81fdb7ce01214ccdf8
Daniel <d.o.rasmusson@gmail.com>
2024-12-25 17:06:20 +0100

    Day 14 and added project for day 15

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,2 +1,147 @@
-﻿var input = File.ReadAllText("Input.txt");
-Console.WriteLine($"{input}");
+﻿var input = File.ReadAllLines("Input.txt");
+var partOne = PartOne(input);
+Console.WriteLine($"Part One: {partOne}");
+
+var partTwo = PartTwo(input);
+Console.WriteLine($"Part Two: {partTwo}");
+int PartOne(string[] input)
+{
+    var result = 0;
+
+    var configs = ParseInput(input);
+
+    foreach (var config in configs)
+    {
+        var cost = SearchForCheapest(config);
+
+        if (cost is not null) result += cost.Value;
+    }
+
+    return result;
+}
+
+long PartTwo(string[] input)
+{
+    var result = 0L;
+
+    var configs = ParseInput(input);
+
+    foreach (var config in configs)
+    {
+        var cost = SearchForCheapest2(config);
+
+        result += cost;
+    }
+
+    return result;
+}
+
+long SearchForCheapest2(ClawMachineConfig config)
+{
+        long aX = config.ButtonA.X;
+        long aY = config.ButtonA.Y;
+        long bX = config.ButtonB.X;
+        long bY = config.ButtonB.Y;
+        
+        var prizeX = config.Prize.X + 10000000000000;
+        var prizeY = config.Prize.Y + 10000000000000;
+
+        var leftSide = (aX * bY) - (aY * bX);
+        var rightSide = (prizeX * bY) - (prizeY * bX);
+        
+        if (rightSide % leftSide != 0)
+            return 0;
+            
+        var buttonAPresses = rightSide / leftSide;
+        
+        var remainingDistance = prizeX - (aX * buttonAPresses);
+        
+        if (remainingDistance % bX != 0)
+            return 0;
+            
+        var buttonBPresses = remainingDistance / bX;
+        
+        if (buttonAPresses < 0 || buttonBPresses < 0)
+            return 0;
+        
+        return (buttonAPresses * 3) + buttonBPresses;
+}
+
+int? SearchForCheapest(ClawMachineConfig config)
+{
+    var costs = new List<int>();
+    
+    var maxA = 100;
+    var maxB = 100;
+    
+    for (int a = 0; a <= maxA; a++)
+    {
+        for (int b = 0; b <= maxB; b++)
+        {
+            int x = a * config.ButtonA.X + b * config.ButtonB.X;
+            int y = a * config.ButtonA.Y + b * config.ButtonB.Y;
+
+            if (x == config.Prize.X && y == config.Prize.Y)
+            {
+                int cost = (3 * a) + b;
+                costs.Add(cost);
+            }
+        }
+    }
+
+    if (costs.Count == 0) return null;
+    
+    return costs.Min();
+}
+
+List<ClawMachineConfig> ParseInput(string[] strings)
+{
+    var result = new List<ClawMachineConfig>();
+    var config = new ClawMachineConfig();
+    for (int i = 0; i < strings.Length; i++)
+    {
+        var d = i % 4;
+
+        if (d == 0)
+        {
+            var x = int.Parse(strings[i].Split("X+")[1].Split(",")[0]);
+            var y = int.Parse(strings[i].Split("Y+")[1]);
+
+            config.ButtonA = new Coord(x, y);
+        }
+        if (d == 1)
+        {
+            var x = int.Parse(strings[i].Split("X+")[1].Split(",")[0]);
+            var y = int.Parse(strings[i].Split("Y+")[1]);
+
+            config.ButtonB = new Coord(x, y);
+        }
+        if (d == 2)
+        {
+            var x = int.Parse(strings[i].Split("X=")[1].Split(",")[0]);
+            var y = int.Parse(strings[i].Split("Y=")[1]);
+
+            config.Prize = new Coord(x, y);
+        }
+
+        if (d == 3)
+        {
+            result.Add(config);
+            config = new ClawMachineConfig();
+        }
+    }
+    result.Add(config);
+    return result;
+}
+class ClawMachineConfig
+{
+    public Coord ButtonA { get; set; }
+    public Coord ButtonB { get; set; }
+    public Coord Prize { get; set; }
+
+}
+
+record Coord(int X, int Y)
+{
+    public static Coord operator +(Coord a, Coord b) => new(a.X + b.X, a.Y + b.Y);
+}
---
+++ Day14/Program.cs
@@ -0,0 +1,141 @@
+﻿var input = File.ReadAllLines("Input.txt");
+var robots = ParseRobots(input);
+var partOne = PartOne(input);
+Console.WriteLine($"Part One: {partOne}");
+var partTwo = PartTwo(input);
+Console.WriteLine($"Part Two: {partTwo}");
+int PartOne(string[] input)
+{
+    var robots = ParseRobots(input);
+    var maxX = 100;
+    var maxY = 102;
+
+    for (int i = 0; i < 100; i++)
+    {
+        foreach (var robot in robots)
+            Blink(robot, maxX, maxY);
+    }
+
+    var result = 1;
+    for (int i = 1; i <= 4; i++)
+        result *= CountRobotsInQuadrant(i, robots, maxX, maxY);
+    
+    return result;
+}
+
+int PartTwo(string[] input)
+{
+    var robots = ParseRobots(input);
+    var maxX = 100;
+    var maxY = 102;
+
+    var robotCount = robots.Count;
+    var positions = robots.Select(r => r.Position).ToHashSet();
+    var i = 0;
+    
+    while (robotCount != positions.Count)
+    {
+        i++;
+        foreach (var robot in robots)
+            Blink(robot, maxX, maxY);
+        
+        positions = robots.Select(r => r.Position).ToHashSet();
+        if (positions.Count == robotCount)
+        {
+            PrintToConsole(robots, maxX, maxY, i);
+            return i;
+        }
+    }
+
+    return i;
+}
+
+void PrintToConsole(List<Robot> robots, int maxX, int maxY, int iteration)
+{
+    var coordSet = robots.Select(r => r.Position).ToList();
+
+    var coordChar = '#';
+    var empty = '.';
+    
+    for (int y = 0; y <= maxY; y++)
+    {
+        for (int x = 0; x <= maxX; x++)
+        {
+            if (coordSet.Contains(new Coord(x, y)))
+                Console.Write(coordChar);
+            else
+                Console.Write(empty);
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine($"Iteration: {iteration}");
+}
+
+int CountRobotsInQuadrant(int quadrant, List<Robot> robots, int maxX, int maxY)
+{
+    if (quadrant == 1)
+    {
+        return robots.Count(r => r.Position.X < maxX / 2 && r.Position.Y < maxY / 2);
+    }
+
+    if (quadrant == 2)
+    {
+        return robots.Count(r => r.Position.X > maxX / 2 && r.Position.Y < maxY / 2);
+    }
+    
+    if (quadrant == 3)
+    {
+        return robots.Count(r => r.Position.X < maxX / 2 && r.Position.Y > maxY / 2);
+    }
+    
+    if (quadrant == 4)
+    {
+        return robots.Count(r => r.Position.X > maxX / 2 && r.Position.Y > maxY / 2);
+    }
+
+    return 0;
+}
+
+void Blink(Robot robot, int maxX, int maxY)
+{
+    var newX = Wrap(robot.Position.X + robot.Velocity.X, maxX + 1);
+    var newY = Wrap(robot.Position.Y + robot.Velocity.Y, maxY + 1);
+    
+    robot.Position = new Coord(newX, newY);
+}
+
+int Wrap(int x, int max) => ((x % max) + max) % max;
+
+List<Robot> ParseRobots(string[] input)
+{
+    var robots = new List<Robot>();
+
+    foreach (var line in input)
+    {
+        var pV = line.Split(" ");
+        var pC = pV[0].Split("=")[1];
+        var pX = int.Parse(pC.Split(",")[0]);
+        var pY = int.Parse(pC.Split(",")[1]);
+        
+        var vC = pV[1].Split("=")[1];
+        var vX = int.Parse(vC.Split(",")[0]);
+        var vY = int.Parse(vC.Split(",")[1]);
+        var robot = new Robot
+        {
+            Position = new Coord(pX, pY),
+            Velocity = new Coord(vX, vY)
+        };
+        robots.Add(robot);
+    }
+    
+    return robots;
+}
+
+class Robot
+{
+    public Coord Position { get; set; }
+    public Coord Velocity { get; init; }
+}
+
+record Coord(int X, int Y);
